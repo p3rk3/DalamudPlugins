05b5d0289fc7ab82124e708e5c08c879741dae41
akira0245 <dbkxiang@gmail.com>
2021-04-28 16:53:19 +0800

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -6,7 +6,7 @@ from sys import argv
 from os.path import getmtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
-DOWNLOAD_URL = 'https://https://raw.fastgit.org/akira0245/DalamudPlugins/raw/cn/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://raw.fastgit.org/akira0245/DalamudPlugins/raw/cn/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
